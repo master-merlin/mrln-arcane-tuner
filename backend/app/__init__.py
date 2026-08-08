0b6fef7d8b86becab576dce9319fe60670511c13
master-merlin <info@lumineszenz.com>
2026-08-08 18:54:17 +0200

    chore: bump version to 0.7.9-beta

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,4 +7,4 @@ from app.core.compat import apply_diffusers_patches, apply_hpsv2_patches  # noqa
 apply_diffusers_patches()
 apply_hpsv2_patches()
 
-__version__ = "0.7.8-beta"
+__version__ = "0.7.9-beta"
